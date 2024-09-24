a3e12d490928ac50865a7fd9ac56af36f13238df
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-24 16:07:57 +0530

    Finished styling the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import terry from "../assets/terry.png"
 function Hero() {
   return (
-    <section>
-        <div>
-            <h1>Hey There <span>I&apos;m Terry</span></h1>
-            <p>I craft intuitive designs that connect with users and drive results. As a multidisciplinary designer, I bring ideas to life through a perfect blend of creativity, strategy, and user-centered design.</p>
-            <button>Lets talk</button>
+    <section className="relative -mt-24">
+        <div  className="absolute top-1/4">
+            <h1 className="text-[7.5rem] font-extrabold leading-none">Hey There,<br /><span className="text-project-green">I&apos;m Terry</span></h1>
+            <p className="mt-3 max-w-[454px] text-xl text-project-gray">I craft intuitive designs that connect with users and drive results. As a multidisciplinary designer, I bring ideas to life through a perfect blend of creativity, strategy, and user-centered design.</p>
+            <button className="text-2xl text-white py-[10px] px-[35px] bg-project-green mt-[33px]">Lets talk</button>
         </div>
-        <img src={terry} alt="A smiling man with a gray T-shirt with long curly hair" />
+        <img className="ml-auto" src={terry} alt="A smiling man with a gray T-shirt with long curly hair" />
     </section>
   )
 }
