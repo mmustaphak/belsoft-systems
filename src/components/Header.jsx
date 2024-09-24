513f927e01615181af988f29b533c80c364647f1
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-24 15:13:38 +0530

    Finished styling the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,24 @@
 export default function Header() {
     return (
-        <header>
-            <nav>
+        <header className="px-8 py-4 shadow-[0px_0.3px_#75687E] bg-white/10 backdrop-opacity-50">
+            <nav className="flex justify-between items-center max-w-[1260px] mx-auto">
                 <div>
-                    <a href="">
-                        <div></div>
-                        <h1>Logo</h1>
+                    <a className="flex items-center" href="">
+                        <div className="mr-[14px] size-[66px] bg-project-green rounded-full"></div>
+                        <h1 className="text-[36px] font-semibold">Logo</h1>
                     </a>
                 </div>
-                <ul>
-                    <a href="">
+                <ul className="flex">
+                    <a className="mr-[30px] text-xl font-bold" href="">
                         <li>Home</li>
                     </a>
-                    <a href="">
+                    <a className="mr-[30px] text-xl text-project-gray" href="">
                         <li>Services</li>
                     </a>
-                    <a href="">
+                    <a className="mr-[30px] text-xl text-project-gray" href="">
                         <li>Portfolio</li>
                     </a>
-                    <a href="">
+                    <a className="text-project-gray text-xl" href="">
                         <li>Contact</li>
                     </a>
                 </ul>
