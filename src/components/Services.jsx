0d02ac45e98d2fd1b142a416fea40b4f976b4f75
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-24 16:27:48 +0530

    Finished styling the basic markup for the services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,14 +2,14 @@ import cone from "../assets/cone.png"
 
 export default function Services() {
     return (
-        <section>
-            <h1>My Services</h1>
+        <section className="mt-[108px]">
+            <h1 className="font-bold text-[2.5rem] text-project-green">My Services</h1>
             <div>
-                <div>
-                    <img src={cone} alt=" " />
+                <div className="flex py-16 px-[41px]">
+                    <img className="object-scale-down max-h-[5rem] mr-[42px]" src={cone} alt=" " />
                     <div>
-                        <h2>Art Direction</h2>
-                        <p>I lead creative teams to execute cohesive visual concepts, overseeing the entire design process to ensure alignment with brand identity and project goals.</p>
+                        <h2 className="font-semibold text-[1.6875rem]">Art Direction</h2>
+                        <p className="max-w-[455px] text-project-gray text-xl">I lead creative teams to execute cohesive visual concepts, overseeing the entire design process to ensure alignment with brand identity and project goals.</p>
                     </div>
                 </div>
             </div>
