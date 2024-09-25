5569ab7a09912ce86dab7ed91e27e7534d9881a1
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-25 15:51:03 +0530

    Extracted a componet out of the ProjectCard

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,20 +1,29 @@
 import nike from "../assets/nike.png"
+
+
 export default function MyWork() {
+
+    function ProjectCard({ title, description, img }) {
+        return (
+            <div className="max-w-[404px]">
+                <img src={img} alt="" />
+                <div className="flex justify-between mt-6">
+                    <div>
+                        <h3 className="font-semibold text-xl">{title}</h3>
+                        <p className="text-project-gray text-[17px]">{description}</p>
+                    </div>
+                    <button className="px-[10px] py-[5px] font-semibold text-white bg-project-green">Case Study</button>
+                </div>
+            </div>
+
+        )
+    }
+
+
     return (
         <section className="mt-[42px]">
             <h2 className="text-project-green font-semibold text-[2.5rem]">My Work</h2>
-            <div className="mt-[42px]">
-                <div className="max-w-[404px]">
-                    <img src={nike} alt="" />
-                    <div className="flex justify-between mt-6">
-                        <div>
-                            <h3 className="font-semibold text-xl">Project Name</h3>
-                            <p className="text-project-gray text-[17px]">Description (i.e Nike Photoshoot)</p>
-                        </div>
-                        <button className="px-[10px] py-[5px] font-semibold text-white bg-project-green">Case Study</button>
-                    </div>
-                </div>
-            </div>
+            <ProjectCard title="Project Name" description="Description (i.e Nike Photoshoot)" img={nike} />
         </section>
     )
 }
