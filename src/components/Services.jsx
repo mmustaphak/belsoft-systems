044fbc623001b4d313f388cfa7579af600977cd2
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-25 15:24:31 +0530

    Extracted a component out of the ServicesCard

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,17 +1,24 @@
 import cone from "../assets/cone.png"
 
 export default function Services() {
+    function ServicesCard({img, title, description}) {
+        return (
+            <div className="flex py-16 px-[41px]">
+                <img className="object-scale-down max-h-[5rem] mr-[42px]" src={img} alt=" " />
+                <div>
+                    <h2 className="font-semibold text-[1.6875rem]">{title}</h2>
+                    <p className="max-w-[455px] text-project-gray text-xl">{description}</p>
+                </div>
+            </div>
+
+        )
+    }
+
     return (
         <section className="mt-[108px]">
             <h1 className="font-bold text-[2.5rem] text-project-green">My Services</h1>
             <div>
-                <div className="flex py-16 px-[41px]">
-                    <img className="object-scale-down max-h-[5rem] mr-[42px]" src={cone} alt=" " />
-                    <div>
-                        <h2 className="font-semibold text-[1.6875rem]">Art Direction</h2>
-                        <p className="max-w-[455px] text-project-gray text-xl">I lead creative teams to execute cohesive visual concepts, overseeing the entire design process to ensure alignment with brand identity and project goals.</p>
-                    </div>
-                </div>
+                <ServicesCard img={cone} title="Art Direction" description="I lead creative teams to execute cohesive visual concepts, overseeing the entire design process to ensure alignment with brand identity and project goals."/>
             </div>
         </section>
     )
