2acde3f0c8e22a8cb294673c5e213b48f4428bfc
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-25 14:41:52 +0530

    Finsihed styling the Project card in the MyWork section

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -1,17 +1,20 @@
+import nike from "../assets/nike.png"
 export default function MyWork() {
-  return (
-    <section>
-        <h2>My Work</h2>
-        <div>
-            <img src="" alt="" />
-            <div>
-                <div>
-                    <h3>Project Name</h3>
-                    <p>Description (i.e Nike Photoshoot)</p>
+    return (
+        <section className="mt-[42px]">
+            <h2 className="text-project-green font-semibold text-[2.5rem]">My Work</h2>
+            <div className="mt-[42px]">
+                <div className="max-w-[404px]">
+                    <img src={nike} alt="" />
+                    <div className="flex justify-between mt-6">
+                        <div>
+                            <h3 className="font-semibold text-xl">Project Name</h3>
+                            <p className="text-project-gray text-[17px]">Description (i.e Nike Photoshoot)</p>
+                        </div>
+                        <button className="px-[10px] py-[5px] font-semibold text-white bg-project-green">Case Study</button>
+                    </div>
                 </div>
-                <button>Case Study</button>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
