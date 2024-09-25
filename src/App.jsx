4f82a56b14cf689e125b6f3d9d074a7ace2348ca
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-25 15:12:59 +0530

    Added the layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Layout from "./components/Layout";
 import MyWork from "./components/MyWork";
 import Services from "./components/Services";
 
@@ -8,10 +9,12 @@ export default function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <Services />
-      <MyWork /> 
-      <Footer /> 
+      <Layout>
+        <Hero />
+        <Services />
+        <MyWork />
+        <Footer />
+      </Layout>
     </>
   )
 }
