4066f8982bc6629d1effd06263f915d70a60cdd5
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-25 14:31:39 +0530

    Added the 'My Work' section with the basic markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import MyWork from "./components/MyWork";
 import Services from "./components/Services";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Hero />
       <Services />
+      <MyWork />  
     </>
   )
 }
---
+++ src/components/MyWork.jsx
@@ -0,0 +1,17 @@
+export default function MyWork() {
+  return (
+    <section>
+        <h2>My Work</h2>
+        <div>
+            <img src="" alt="" />
+            <div>
+                <div>
+                    <h3>Project Name</h3>
+                    <p>Description (i.e Nike Photoshoot)</p>
+                </div>
+                <button>Case Study</button>
+            </div>
+        </div>
+    </section>
+  )
+}
