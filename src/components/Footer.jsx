bf5df32141d2e89dc83d0c79ed455c8347b682af
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-25 16:08:02 +0530

    added footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ export default function Footer() {
                 <div className="max-w-[598px]">
                     <p>Let&apos;s create something extraordinary together. Whether you have a project in mind or just want to chat, feel free to reach out!</p>
                     <div className="flex justify-between mt-[15px] max-w-[156px]">
-                        <a href=""><img src={instagram} alt="" /></a>
-                        <a href=""><img src={dribble} alt="" /></a>
-                        <a href=""><img src={instagram} alt="" /></a>
+                        <a href="https://www.instagram.com/animaapp/?hl=en"><img src={instagram} alt="" /></a>
+                        <a href="https://dribbble.com/animaapp"><img src={dribble} alt="" /></a>
+                        <a href="https://www.behance.net/Anima_design"><img src={behance} alt="" /></a>
                     </div>
                     <form className="flex flex-col mt-[10px]" action="">
                         <input className="px-6 py-4 bg-[#FAFAFA] placeholder:text-[#2D2D2D] " type="text" name="Name" placeholder="Name" />
@@ -20,8 +20,8 @@ export default function Footer() {
                         <submit className="w-fit mt-[34px] py-[10px] px-[35px] font-medium text-2xl text-white bg-project-green">Submit</submit>
                     </form>
                 </div>
-                <p className="font-extrabold leading-none text-[8.75rem]">Thank <br/><span className="ml-[137px] text-project-green">You.</span></p>
+                <p className="font-extrabold leading-none text-[8.75rem]">Thank <br /><span className="ml-[137px] text-project-green">You.</span></p>
             </div>
-        </footer>
+        </footer >
     )
 }
